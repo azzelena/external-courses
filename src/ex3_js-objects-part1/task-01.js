b2c95627916d14466b99e154ddaeedccbdd6da81
Елена <azzelena@MacBook-Pro-Elena.local>
2018-10-07 11:16:14 +0300

    are ready

---
+++ src/ex3_js-objects-part1/task-01.js
@@ -0,0 +1,7 @@
+"use strict";
+var worker = {};
+        worker.name = 'Anna';
+        worker.age = 21;
+        worker.projects = ['css_style', 'func_js_for_buttons','mobile app'];
+            delete worker.projects;
+console.log(worker);
