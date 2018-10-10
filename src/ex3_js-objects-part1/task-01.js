351c8a22d3edccd2d7848551021726f953f1c472
Елена <azzelena@MacBook-Pro-Elena.local>
2018-10-10 19:32:20 +0300

    delete

--- src/ex3_js-objects-part1/task-01.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var worker = {};
- worker.name = 'Anna';
-        worker.age = 21;
-        worker.projects = ['css_style', 'func_js_for_buttons','mobile app'];
-            delete worker.projects;
-console.log(worker);
--- src/ex3_js-objects-part1/task-06.js
+++
@@ -1,5 +0,0 @@
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
- }
- module.exports = deepClone;
- console.log(deepClone({1: 'qq', 2: 'ff'}));
