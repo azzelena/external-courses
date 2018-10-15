498cc6e8a3609c9457ec350f1fc868f476a95c7d
Елена <azzelena@MacBook-Pro-Elena.local>
2018-10-15 09:46:42 +0300

    ready

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,15 +1,9 @@
 "use strict";
-var res = 0;
 function findPropinPrototype(propName,obj){
-    var objPrototype = Object.create(obj);
-    objPrototype.myName = 'Elena';
-    objPrototype.age = '25';
-    for(propName in objPrototype) {
-        if(objPrototype.hasOwnProperty(propName)) {
-          res = res + 1;  
-         }
-     }
-  return res;
+        if(!objPrototype.hasOwnProperty(propName) && propName in obj) {
+        return obj[propName]; 
+    }
+  
 }
 module.exports = findPropinPrototype;
 console.log(findPropinPrototype('age',{'name': 'Elena', 'age': '21'}));
